7567ab205ca37cc2dacc9fcf20b39b6764976161
RAMIZA123 <ramizamalagi@gmail.com>
2025-02-28 14:01:41 +0530

    ecom.frontend

---
+++ main.js
@@ -0,0 +1,10 @@
+const calculator=required("./calculator");
+
+let num1 =5;
+let num2 =10;
+
+console.log(calculator.add(num1,num2));
+console.log(calculator.subtract(num1,num2));
+console.log(calculator.multipily(num1,num2));
+console.log(calculator.divide(num1,num2));
+                    
