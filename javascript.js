96d9dcf327b95cc0e867c227996425e5158174cd
RAMIZA123 <ramizamalagi@gmail.com>
2025-02-28 13:57:50 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,15 @@
+
+let username;
+
+console.log("hello",+username+"!");
+
+var username="smith";
+)
+function add(a,b){
+    return a+b;
+}
+
+const addArrow=(a,b)=> a+b;
+cosnt addArrow2=(a,b)=>{
+    return a+b;
+}
